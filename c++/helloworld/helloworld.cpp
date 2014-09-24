f151324e0a14aa3601e3382e0cb6e36681301e62
pjotr <p.lasschuit@xs4all.nl>
2014-09-24 17:42:08 +0200

    1st c++ programm + converter script created and completed

---
+++ c++/helloworld/helloworld.cpp
@@ -0,0 +1,19 @@
+#include  <iostream>
+
+using namespace std;            //zorgt ervoor dat je niet opnieuw std hoeft aan
+                                // te roepen
+
+int main(int argc, char** argv) //er is maar 1 main funtie 
+                                //(niet meer, niet minder), 
+                                //die wordt aangeroepen bij het 
+                                //opstarten van de code, geeft 
+                                //altijd een int terug.
+{
+     cout << "Hello World" << endl;  //kleinder dan tekens voor routing, 
+                                     //endl betekent regel afsluiten. 
+                                     //cout betekent de regel
+                                     //die daarna staat (<<) in 
+                                     //de console stoppen
+  return 0;
+}
+
